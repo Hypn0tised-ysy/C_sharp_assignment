036303fb973a68938b977dad286ff7dd7c3b6a11
Shunyu Ye <145466888+Hypn0tised-ysy@users.noreply.github.com>
2025-02-23 19:44:56 +0800

    添加项目文件。

---
+++ assignmentForC#/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignmentForC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("yes, I'm 叶顺雨");
+        }
+    }
+}
